ed6908a0a7fa06189ef9bc0208293abd855fca4d
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-18 20:13:57 +0200

    feat(fe): design service

--- fe/src/app/website-design-service/page.tsx
+++ fe/src/app/website-design-service/page.tsx
@@ -1,15 +1,65 @@
-import { BlogCard } from "@/components/blog/card";
-import { getBlogPosts } from "@/lib/api/blog";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
 
 export default async function Page() {
-  const data = await getBlogPosts();
   return (
-    <div className="flex flex-col p-8 gap-8">
-      {data.data.map((category) =>
-        category.data.posts.map((image) => (
-          <BlogCard key={image.id} blogPost={image} />
-        ))
-      )}
+    <div className="flex flex-col p-8 gap-8 *:w-fit *:mx-auto">
+      <h1 className="text-3xl font-bold text-center">
+        Website Design Services Let us tell your story
+      </h1>
+      <p>
+        We offer professional website design services tailored to elevate your
+        brand, enhance user experience and elevate your brand.
+      </p>
+      <p>
+        A professionally designed website is essential for any business seeking
+        to make a strong digital impression. We specialize in creating visually
+        stunning, user-friendly websites tailored to your unique brand. ‍
+      </p>
+      <Button asChild className="w-fit py-6 text-xl mx-auto">
+        <Link href="/contact">Get a quote</Link>
+      </Button>
+      <p className="text-lg text-center">
+        We create clean, minimalist website designs for realtors, seamlessly
+        integrating IDX data feeds to showcase property listings with style and
+        functionality.
+      </p>
+      <Image
+        src="/straight.webp"
+        alt="Real estate website design"
+        height={800}
+        width={800}
+        className="w-full"
+      />
+      <p className="text-center">
+        For photographers, we craft galleries that highlight your artistry with
+        high-resolution visuals and seamless navigation.
+      </p>
+      <p className="text-center">
+        For interior designers, we design layouts that reflect your aesthetic,
+        integrating project showcases, client testimonials, and interactive
+        features.
+      </p>
+      <Image
+        src="/old.webp"
+        alt="Real estate website design"
+        height={800}
+        width={800}
+        className="w-full"
+      />
+      <p className="text-center">
+        Whether you’re an established business looking to enhance your online
+        presence or a photographer or interior designer aiming to showcase your
+        portfolio, we deliver websites that captivate and convert.
+      </p>
+      <p className="text-center">
+        Our websites are mobile-friendly, SEO-optimized, and customized to help
+        you attract and engage your target audience. Let us transform your
+        vision into a website that not only represents your business but also
+        drives success.
+      </p>
     </div>
   );
 }
