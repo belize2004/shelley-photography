c9a3e1b079c3baf140ac4b55fd1871dbe2c98b66
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-19 08:16:57 +0200

    dirty hack, have a better idea ?

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { getPhotos } from "@/lib/api/categories";
 import { IMAGE_BASE_URL } from "@/lib/const";
+import { Photo } from "@/lib/types";
 import Image from "next/image";
 import { useQuery } from "react-query";
 
@@ -9,21 +10,32 @@ export default function Home() {
     queryKey: "categories",
     queryFn: getPhotos,
   });
+
+  const photoIds = new Set();
+  const uniquePhotos: Photo[] = [];
+
+  data?.data.forEach((category) => {
+    category.photos?.forEach((photo) => {
+      if (!photoIds.has(photo.documentId)) {
+        photoIds.add(photo.documentId);
+        uniquePhotos.push(photo);
+      }
+    });
+  });
+
   return (
-    <div className="flex flex-wrap p-8">
-      {data?.data.map((category) =>
-        category.photos?.map((image) => (
-          <div key={image.id} className="m-2">
-            <Image
-              src={IMAGE_BASE_URL + image.url}
-              width={300}
-              height={300}
-              alt="Image"
-              className="rounded-xl"
-            />
-          </div>
-        ))
-      )}
+    <div className="flex flex-wrap p-8 mx-auto w-full items-center justify-center">
+      {uniquePhotos.map((image) => (
+        <div key={image.id} className="m-2">
+          <Image
+            src={IMAGE_BASE_URL + image.url}
+            width={300}
+            height={300}
+            alt="Image"
+            className="rounded-xl"
+          />
+        </div>
+      ))}
     </div>
   );
 }
