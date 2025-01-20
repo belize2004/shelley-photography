d4675ceccb3eff171ffe6f7570b58188eff5f670
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-20 14:25:08 +0200

    I dont know what I am doing

--- fe/src/app/engagement-sessions/page.tsx
+++ fe/src/app/engagement-sessions/page.tsx
@@ -3,6 +3,7 @@ import { BlogCard } from "@/components/blog/card";
 import { getPhotos } from "@/lib/api/categories";
 import { IMAGE_BASE_URL } from "@/lib/const";
 import Image from "next/image";
+import Masonry from "react-masonry-css";
 import { useQuery } from "react-query";
 
 export default function Page() {
@@ -10,9 +11,20 @@ export default function Page() {
     queryKey: "categories",
     queryFn: getPhotos,
   });
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   return (
     <>
-      <div className="flex flex-wrap p-8">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="flex w-auto"
+        columnClassName="bg-clip-padding px-2"
+      >
         {data?.data
           .filter((c) => c.name === "engagement sessions")
           .map((category) =>
@@ -28,7 +40,7 @@ export default function Page() {
               </div>
             ))
           )}
-      </div>
+      </Masonry>
       <div className="flex flex-col p-8 gap-8">
         {data?.data
           .filter((c) => c.name === "engagement sessions")
