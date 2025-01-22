0c656007c4eda56439ac8de61089642bed37ed2f
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-23 01:02:20 +0200

    Fixed the fuck out of #512!

--- fe/src/app/real-estate/page.client.tsx
+++ fe/src/app/real-estate/page.client.tsx
@@ -47,48 +47,48 @@ export default function PageClient() {
         alt="Ratings"
         className="my-8 w-full"
       />
-      <div className="flex flex-col p-8 gap-8">
+      <div className="flex flex-col p-8 gap-2">
         <h1 className="text-2xl">REAL ESTATE PRICING</h1>
         <div>
           Transform your real estate listings with professional photography.
           Professional photography isn’t just a detail—it’s a difference-maker.
-          Here’s why it matters: <div className="py-2 block" /> Sell Faster.
+          Here’s why it matters: <div className="py-1 block" /> Sell Faster.
           Homes with professional photography sell 32% faster. On average, they
           spend 89 days on the market compared to 123 days for those with
-          standard or low-quality images. <div className="py-2 block" /> Achieve
+          standard or low-quality images. <div className="py-1 block" /> Achieve
           Higher Prices. Properties with high-quality photos command between
           $934 and $116,076 more than comparable listings with amateur
-          photography. <div className="py-2 block" /> Capture More Attention.
+          photography. <div className="py-1 block" /> Capture More Attention.
           Listings with professional images receive 118% more online views,
           putting your property in front of more potential buyers.{" "}
-          <div className="py-2 block" /> Create Perceived Value. Homes with
+          <div className="py-1 block" /> Create Perceived Value. Homes with
           professionally edited photos see a 47% higher asking price per square
-          foot, enhancing their market appeal. <div className="py-2 block" />{" "}
+          foot, enhancing their market appeal. <div className="py-1 block" />{" "}
           Make a First Impression That Lasts. Your photos are the first thing
           buyers see. Professionally shot images captivate and invite them to
           explore further.
-          <div className="py-2 block" /> Highlight Every Detail. Angles,
+          <div className="py-1 block" /> Highlight Every Detail. Angles,
           lighting, and composition are expertly used to showcase every unique
           feature of your property, from natural light to architectural
           highlights.
-          <div className="py-2 block" /> Stand Out in a Competitive Market. In a
+          <div className="py-1 block" /> Stand Out in a Competitive Market. In a
           sea of listings, professional photography helps yours rise above,
           especially for luxury homes where quality visuals are essential.
-          <div className="py-2 block" /> Effortless for Agents. We handle the
+          <div className="py-1 block" /> Effortless for Agents. We handle the
           photography so you can focus on selling. Quick setups, efficient
           shoots, and fast turnarounds keep you ahead.
-          <div className="py-2 block" /> Showcase the Lifestyle. Beyond a house,
+          <div className="py-1 block" /> Showcase the Lifestyle. Beyond a house,
           sell the dream. From cozy outdoor spaces to inviting living areas, our
           photography paints a picture buyers can step into.
-          <div className="py-2 block" /> ‍ Precision Technology. With
+          <div className="py-1 block" /> ‍ Precision Technology. With
           state-of-the-art cameras and professional editing, every photo is
           crisp, vibrant, and meticulously polished. ‍
-          <div className="py-2 block" /> Why choose professional photography?
+          <div className="py-1 block" /> Why choose professional photography?
           Because every listing deserves the attention it gets. Elevate your
           brand, attract more buyers, and close deals faster.
-          <div className="py-2 block" /> Same-day appointments.24-48 hour
+          <div className="py-1 block" /> Same-day appointments.24-48 hour
           turnaround.High-quality images that move properties.
-          <div className="py-2 block" /> 1 to 15 images $299 ‍
+          <div className="py-1 block" /> 1 to 15 images $299 ‍
         </div>
         <Link
           href="https://shelleybressmanphotography.pixieset.com/booking/real-estate-photos-1-15-images"
