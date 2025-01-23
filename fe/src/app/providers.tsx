139000bb518236e810527803f741c8f9588943ed
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-23 17:36:30 +0200

    Herpderp, shoulda check if it does really compile.

--- fe/src/app/providers.tsx
+++ fe/src/app/providers.tsx
@@ -1,10 +1,23 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 "use client";
 
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5000,
+            //@ts-expect-error
+            cacheTime: 0,
+            refetchOnWindowFocus: true,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
