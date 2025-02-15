dfddf0192bac527cdfc2d59a4ca0b2560a7b8106
AmrTamer23 <amrtamer23mj@gmail.com>
2025-02-16 00:32:15 +0200

    minor changes

--- fe/src/components/gallery.tsx
+++ fe/src/components/gallery.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useMemo } from "react";
-import Masonry from "react-masonry-css";
+import { Masonry } from "masonic";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { home } from "@/lib/api/categories";
 import { IMAGE_BASE_URL } from "@/lib/const";
@@ -35,27 +35,46 @@ export function Gallery({ isMobile }: GalleryProps) {
   const photos = useMemo(() => data.data[0]?.photos || [], [data]);
 
   return (
+    // <Masonry
+    //   breakpointCols={breakpointCols}
+    //   className="flex w-auto"
+    //   columnClassName="bg-clip-padding px-2 first:pl-4 last:pr-4 py-4"
+    // >
+    //   {photos.map((image, idx) => (
+    //     <div key={image.id} className="mb-4 relative group">
+    //       <Image
+    //         src={IMAGE_BASE_URL + image.url}
+    //         width={image.width || 1200}
+    //         height={image.height || 800}
+    //         alt={image.alt || "Gallery image"}
+    //         className="rounded-xl w-full h-auto transition-transform duration-300 group-hover:scale-[1.02]"
+    //         priority={idx < 4}
+    //         loading={idx < 4 ? "eager" : "lazy"}
+    //         placeholder="blur"
+    //         blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+    //         sizes="(max-width: 500px) 100vw, (max-width: 1100px) 50vw, 33vw"
+    //       />
+    //     </div>
+    //   ))}
+    // </Masonry>
     <Masonry
-      breakpointCols={breakpointCols}
-      className="flex w-auto"
-      columnClassName="bg-clip-padding px-2 first:pl-4 last:pr-4 py-4"
-    >
-      {photos.map((image, idx) => (
-        <div key={image.id} className="mb-4 relative group">
+      items={photos}
+      columnCount={breakpointCols.default}
+      render={({ index, data: { width, height, url, alternativeText } }) =>
+        <div className=" relative group p-2">
           <Image
-            src={IMAGE_BASE_URL + image.url}
-            width={image.width || 1200}
-            height={image.height || 800}
-            alt={image.alt || "Gallery image"}
+            src={IMAGE_BASE_URL + url}
+            width={width || 1200}
+            height={height || 800}
+            alt={alternativeText || "Gallery image"}
             className="rounded-xl w-full h-auto transition-transform duration-300 group-hover:scale-[1.02]"
-            priority={idx < 4}
-            loading={idx < 4 ? "eager" : "lazy"}
+            priority={index < 4}
+            loading={index < 4 ? "eager" : "lazy"}
             placeholder="blur"
             blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
             sizes="(max-width: 500px) 100vw, (max-width: 1100px) 50vw, 33vw"
           />
         </div>
-      ))}
-    </Masonry>
+      } />
   );
 }
